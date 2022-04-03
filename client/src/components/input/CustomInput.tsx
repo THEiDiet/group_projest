0d33c44a66a6903c718135626c5bd743a207f0e0
Mikalai Zarazaka <golmtz@gmail.com>
2022-04-03 17:07:07 +0300

    create reusable input

---
+++ client/src/components/input/CustomInput.tsx
@@ -0,0 +1,37 @@
+import React from 'react'
+
+import { InputPropsType } from '../../types/InputPropsType'
+
+import styles from './styles/CustomInput.module.scss'
+
+export const CustomInput: React.FC<InputPropsType> = props => {
+  const { label, onClick, icon, type, id, name, onChange, value, ...rest } = props
+  return (
+    <div className={styles.input_container}>
+      <label htmlFor={name} className={styles.text_field_label}>
+        {label}
+      </label>
+      <div>
+        <input
+          required
+          id={id}
+          name={name}
+          type={type}
+          onChange={onChange}
+          value={value}
+          onBlur={rest.onBlur}
+        />
+        {icon && (
+          // eslint-disable-next-line jsx-a11y/click-events-have-key-events,jsx-a11y/no-static-element-interactions
+          <span onClick={onClick}>
+            <img
+              className={styles.icon}
+              src={icon}
+              alt="Button for show/close password"
+            />
+          </span>
+        )}
+      </div>
+    </div>
+  )
+}
--- client/src/pages/login/input/CustomInput.tsx
+++
@@ -1,3 +0,0 @@
-import React from 'react'
-
-export const CustomInput: React.FC<any> = () => <input />
