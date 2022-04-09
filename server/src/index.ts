7de4dd76cd7d276c05a6840a17f5c710e11f9d64
THEiDiet <76583093+THEiDiet@users.noreply.github.com>
2022-04-09 20:27:05 +0700

    server added

---
+++ server/src/index.ts
@@ -0,0 +1,42 @@
+import express from "express";
+import http from "http";
+import socketIo from "socket.io";
+import mongoose from "mongoose";
+import {MongoDBUris, PORT} from "./cnb-1-main/config";
+import {appUse} from "./cnb-1-main/app";
+import {routes} from "./cnb-1-main/routes";
+import {onConnect} from "./cnb-2-features/f-3-social/s-1-controllers/c1-socket";
+// yarn upgrade
+
+const app = express();
+
+appUse(app);
+routes(app);
+
+const server = http.createServer(app);
+const socketServer = socketIo(server);
+
+socketServer.on('connection', onConnect(socketServer));
+
+/////////////////////////////////////////////////////////////////
+
+mongoose.connect(MongoDBUris, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+})
+    .then(() => {
+        console.log("Nya-MongoDB connected successfully");
+
+        const port = process.env.PORT || PORT;
+
+        server.listen(port, () => {
+            console.log("Cards-Nya-back 2.0 listening on port: " + port);
+        });
+    })
+    .catch(e => console.log("Nya-MongoDB connection error: ", {...e}));
+
+process.on("unhandledRejection", (reason, p) => {
+    console.log("Nya-unhandledRejection: ", reason, p);
+});
